8e717d7c988f4b31568ac5a79aeafb661814fd4c
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2025-08-26 09:29:54 +0900

    ちょっと追加

--- koziin/Object/Charactor/Player/State/Enum/PlayerState.h
+++ koziin/Object/Charactor/Player/State/Enum/PlayerState.h
@@ -6,3 +6,10 @@ enum class ePlayerState : unsigned char
 	RUN,	  // ˆÚ“®ó‘Ô
 	NONE // ‰½‚à‚È‚¢
 };
+
+enum class PlayerCommandState {
+	MainCommand, // í‚¤E“¦‚°‚é“™‚ğ‘I‚Ô
+	EnemySelect	 // “G‚ğ‘I‚Ô
+};
+
+PlayerCommandState commandState;
